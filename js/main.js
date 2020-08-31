1f1308eea5744f5af1c3f404092f6e7d4fee3411
henriknii <henrin@hiof.no>
2020-08-31 21:41:21 +0200

    some stuff

--- js/main.js
+++ js/main.js
@@ -1,22 +1,49 @@
+
+
+
 //Global variables
 var modalBg = document.querySelector('.modal-bg');
 
 //Todo objects 
-let myObjTodo;
+
+let myTodos = []
+// Keeping track of id for the todo
+let id = 0;
 //A test button for now
 let button = document.getElementById("createTodoBtn");
 let createTodoCardButton = document.getElementById('modalBtn');
 let closeModalBtn = document.getElementById('closeModalBtn');
 
-//Function to generate todocard based on Objects
-function generateTodoList(){
-    myObjTodo = {title: "test"}
+//Function creating object and storing them in the array
+function createObject(){
+    id++
+    let todo = [];
+    let todoTitle = document.getElementById('inputTitle').value;
+    let description = document.getElementById('textArea').value;
+    let author = document.getElementById('author').value;
+    
+    if(todoTitle && description && author){
+        let myTodoObj = {id:id,title: todoTitle, description: description, author:author}
+        todo.unshift(myTodoObj)
+    
+        closeModal()
+    }
+    else
+    {
+        alert("please enter your title,description and author")
+    }
+}  
+
+
+
+function update(obj){
+
+
     
 }
-
 //Function to generate todo cards
 function generateTodoArticles(obj){
-
+    
     
     //Getting the articleWrapper.
     let articleWrapper = document.getElementById("todoArticles");
@@ -29,20 +56,27 @@ function generateTodoArticles(obj){
     
     //Desecription
     var descr = document.createElement('p')
-    descr.textContent = "Descr goes here"
+    descr.textContent = obj.description
+
+    //Creating delete button
+    var buttonDelete = document.createElement('a')
+    buttonDelete.textContent = "Delete";
+    buttonDelete.className="buttonDelete"
+    buttonDelete.addEventListener("click",(e) =>{
+    deleteTodo(e,obj)
+    })
+
 
 
     //creating complete button
     var buttonComplete = document.createElement('a')
     buttonComplete.textContent = "Complete";
     buttonComplete.className="buttonComplete"
-    buttonComplete.addEventListener("click",alert("test"))
+    buttonComplete.onclick = () =>{
+        completedTask(obj)
+    }
 
-    //Creating delete button
-    var buttonDelete = document.createElement('a')
-    buttonDelete.textContent = "Delete";
-    buttonDelete.className="buttonDelete"
-    buttonDelete.addEventListener("click",deleteTodo)
+  
 
    
 
@@ -52,9 +86,16 @@ function generateTodoArticles(obj){
     article.appendChild(descr)
     article.appendChild(buttonComplete) 
     article.appendChild(buttonDelete)
+}
+
+//Called when a task is completed 
+
+function completedTask(obj){
+ let tableContent = document.getElementById('tableContent')
+ let author = obj.author
+ 
+ tableContent.appendChild
     
-    
-    closeModal();
 }
 
 //Called upon clicking the +todo to open modal for the todo creation window.
@@ -67,17 +108,26 @@ function closeModal(){
 }
 
 //called upon clicking delete
-function deleteTodo(){
-    element = this.parentElement
-    element.remove()
+function deleteTodo(e,obj){
+    
+}
+
+
+
+function getFormattedDate(){
+    let date = new Date()
+    let month = date.getMonth() +1;
+    let day = date.getDate();
+    let year = date.getFullYear()
+
+    return day + "." + month + "." + year
 }
 
 //Called upon creating the todo task.
-createTodoCardButton.addEventListener("click",generateTodoList);
+createTodoCardButton.addEventListener("click",createObject);
 
 //Opening the modal
 button.addEventListener("click", openModal);
 
 //Closing the modal
 closeModalBtn.addEventListener("click",closeModal);
-
